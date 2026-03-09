51f822158c0c5ee26770b62a060d08f7e7da158d
freddster14 <freddycarrillo951@gmail.com>
2026-03-09 16:00:15 -0500

    Update CORS configuration to allow requests from the Vercel deployment URL

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors( {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://mock-book.vercel.app/"],
   credentials: true,
 }))
 app.use(cookieParser())
